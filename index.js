8b96ef64bd7bea75fdef774deb4ca6f42db4260d
Tasso Evangelista <tasso@tassoevan.me>
2017-05-26 01:44:03 -0300

    Apply matrices module

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const calculus = require('./calculus.js');
+const matrices = require('./matrices.js');
 
 const context = {
   variables: {
     F1: 2,
-    F2: 1,
+    F2: 2.1,
     F3: 1
   },
   equations: [
@@ -17,19 +18,6 @@ const error = context => x =>
   context.equations.map(equation => equation(x))
     .reduce((sum, error) => sum + error * error, 0);
 
-const grad = (f, context) => x => {
-  const float = new Float32Array(1);
-  const int = new Int32Array(float.buffer);
-
-  return Object.keys(context.variables).map(variableName => {
-      const x_ = Object.assign({}, x);
-      float[0] = x[variableName];
-      int[0]++;
-      x_[variableName] = float[0];
-      return (f(x_) - f(x))/(x_[variableName] - x[variableName]);
-    });
-};
-
 const J = context => x => {
   const F = context.equations.map(
     equation => X => equation(
@@ -39,51 +27,8 @@ const J = context => x => {
     )
   );
   const X = Object.keys(x).map(k => x[k]);
-  console.log(calculus.J(F)(X));
   return calculus.J(F)(X);
-}
-
-const submatrix = (matrix, i, j) =>
-  matrix.slice(0, i)
-    .map(line => line.slice(0, j).concat(line.slice(j + 1)))
-    .concat(
-      matrix.slice(i + 1)
-        .map(line => line.slice(0, j).concat(line.slice(j + 1)))
-    )
-;
-
-const minor = (matrix, i, j) => det(submatrix(matrix, i, j));
-
-const cofactor = (matrix, i, j) =>
-  ((i + j) % 2 === 0 ? 1: -1) * minor(matrix, i, j);
-
-const det = matrix => {
-  if (matrix.length === 1) {
-    return matrix[0][0];
-  }
-  else {
-    return matrix[0].reduce((sum, a, i) => {
-      return sum + a * cofactor(matrix, 0, i);
-    }, 0);
-  }
 };
-
-const comatrix = matrix =>
-  matrix.map((line, i) => line.map((a, j) => cofactor(matrix, i, j)));
-
-const inv = matrix => {
-  const determinant = det(matrix);
-  if (determinant === 0) {
-    throw new Error('singular matrix');
-  }
-
-  const C = comatrix(matrix);
-
-  return C.map((line, i) => line.map((a, j) => C[j][i]/determinant));
-};
-
-const apply = (matrix, vector) =>
-  matrix.map(line => line.reduce((sum, a, i) => sum + line[i] * vector[i], 0));
 
 const solve = (context, maxIterations = 100, maxError = 1e-8) => {
   if (Object.keys(context.variables).length > context.equations.length) {
@@ -100,10 +45,9 @@ const solve = (context, maxIterations = 100, maxError = 1e-8) => {
     }
 
     const jacobian = J(context)(solution);
-    console.log(jacobian);
-    const invJacobian = inv(jacobian);
+    const invJacobian = matrices.inv(jacobian);
     const errors = context.equations.map(equation => equation(solution));
-    const correction = apply(invJacobian, errors);
+    const correction = matrices.apply(invJacobian, errors);
 
     variableNames.map((variableName, j) => {
         return solution[variableName] - correction[j];
